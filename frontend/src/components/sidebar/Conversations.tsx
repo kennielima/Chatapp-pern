1b6d9b7ae16308e700476ece80d8bf643707bf1d
kennielima <kennylima970@gmail.com>
2025-01-07 15:25:36 +0100

    bug fix

--- frontend/src/components/sidebar/Conversations.tsx
+++ frontend/src/components/sidebar/Conversations.tsx
@@ -1,20 +1,36 @@
 import Conversation from "./Conversation";
 import useGetConversations from "../../hooks/useGetConversations";
 import { getRandomEmoji } from "../../utils/emojis";
+import { DUMMY_CONVERSATIONS } from "../../dummy/dummy";
 
 const Conversations = () => {
 	const { loading, conversations } = useGetConversations()
 
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			{conversations.map((conversation) => (
-				<Conversation
-					key={conversation.id}
-					conversation={conversation}
-					emoji={getRandomEmoji()}
-				/>
-
-			))}
+			{conversations.length === 0 ?
+				(
+					conversations.map((conversation) => (
+						<Conversation
+							key={conversation.id}
+							conversation={conversation}
+							emoji={getRandomEmoji()}
+						/>
+					))
+				) : (
+					DUMMY_CONVERSATIONS.map((conversation) => (
+						<Conversation
+							key={conversation.id}
+							emoji={getRandomEmoji()}
+							conversation={{
+								id:  `${conversation.id}`,
+								fullName:  `${conversation.fullName}`,
+								profilePic: `${conversation.profilePic}`
+							}}
+						/>
+					))
+				)
+			}
 			{loading ? (
 				<span className="loading loading-spinner mx-auto" />
 			) : null}
